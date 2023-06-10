80036f9e7ef4e73c86361cb1651e34c179a355f3
Liana Gelikas <lianang@sapiens.com>
2023-06-10 10:56:19 +0300

    added fb login

--- logic.js
+++ logic.js
@@ -1,10 +1,10 @@
 
-const url = 'https://api.instagram.com/oauth/authorize?client_id=547762730900603&redirect_uri=https://daniellegelikas.github.io/hopto/&scope=user_profile&response_type=code'
+const url = 'https://api.instagram.com/oauth/authorize?client_id=547762730900603&redirect_uri=https://daniellegelikas.github.io/hopto/index&scope=user_profile&response_type=code'
 
 
 async function logJSONData() {
   const response = await fetch(url);
-  const jsonData = await response.json();
+  const jsonData = await response.text();
   console.log(jsonData);
 }
 logJSONData()
